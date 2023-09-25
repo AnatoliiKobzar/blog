d4f649897a295bb4d61219d2d461ceedc50cb2b5
KobzarAnatolii <TohaPirat@gmail.com>
2023-09-25 22:44:45 +0300

    add test post

--- app/page.js
+++ app/page.js
@@ -1,11 +1,25 @@
 'use client';
 
-import BlocCard from '@components/cards/BlogCard';
+// import BlocCard from '@components/cards/BlogCard';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 
 export default function Home() {
-  const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState([
+    {
+      title: 'Введение в Next.js',
+      subheading: 'Основные концепции и преимущества',
+      image: 'https://testrigor.com/wp-content/uploads/2023/04/nextjs-logo-square.png',
+      tag: 'Next.js',
+    },
+    {
+      title: 'Изучение React.js',
+      subheading: 'Основы и преимущества библиотеки React',
+      image:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png',
+      tag: 'React.js',
+    },
+  ]);
   const [selectedTag, setSelectedTag] = useState([]);
   const [error, setError] = useState(null);
 
@@ -34,9 +48,11 @@ export default function Home() {
 
   return (
     <main className="min-h-screen lg:p:20">
-      <h2 className="text-center text-3xl md:text-4xl lg:text-5xl font-bold mb-10 flex justify-center items-center">
-        <span className="text-5xl border-b-3 pb-3 font-bold">Keba Web Dev Blog</span>
-      </h2>
+      <div className="text-center text-3xl md:text-4xl lg:text-5xl font-bold mb-10 flex flex-col justify-center items-center uppercase tracking-widest h-96">
+        <h2 className="text-5xl border-b-4 pb-3 font-bold">Keba Web Dev Blog</h2>
+        <p className="text-lg mt-10">Like, share and subscribe for more content!</p>
+      </div>
+      <h3 className="flex flex-wrap mt-10 gap-4">{[...new Set(posts?.map(post => post.tag))]}</h3>
     </main>
   );
 }
